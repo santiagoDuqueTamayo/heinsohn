1337fce037d8b1f485085b5fec494737dc6a135a
Santiago Duque Tamayo <santiagohdt@gmail.com>
2019-10-31 08:00:07 -0500

    ejercicio resulto con los requerimientos basicos

---
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/MostrarDatos/datos-components.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+/**
+ * @description Componente datos, contiene la logica que representa los datos del usuario 
+ * desarrollados
+ * 
+ * @author Diego Fernando Alvarez Silva <dalvarez@heinsohn.com.co>
+ */
+
+@Component({
+    selector: 'usuario',
+    templateUrl:'./datos-component.html',
+})
+
+export class datosComponent{
+    
+}
+
