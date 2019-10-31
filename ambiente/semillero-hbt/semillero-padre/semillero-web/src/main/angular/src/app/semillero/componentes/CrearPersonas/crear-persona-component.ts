3c6021fb7fd370613c670496d48dd53fc3db8c36
Santiago Duque Tamayo <santiagohdt@gmail.com>
2019-10-31 07:23:04 -0500

    clase primera sesion

---
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/CrearPersonas/crear-persona-component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+/**
+ * @Component({
+  selector: '',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class CrearPersonaComponent {
+  
+  constructor(private router : Router) {
+  }  
+
+  ngOnInit(): void {
+    this.router.navigate(['bienvenida']);
+  }
+}
+
+ */
