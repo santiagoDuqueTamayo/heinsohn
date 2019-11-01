996d08a9e507471ba986b39d9be790217ead5e82
Santiago Duque Tamayo <santiagohdt@gmail.com>
2019-11-01 10:51:23 -0500

    tarea con documentacion

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/GestionarComicPOJOTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/GestionarComicPOJOTest.java
@@ -24,9 +24,25 @@ import com.hbt.semillero.pojo.GestionarComicPOJO;
  */
 public class GestionarComicPOJOTest {
 	
+	/**
+	 * representa una instancia de ComicDTO
+	 */
 	ComicDTO comicDTO=null;
+	/**
+	 * Representa una instancia de la clase GestionarComicPOJO
+	 */
 	GestionarComicPOJO gestionarComicPOJO=null;
+	/**
+	 * Lista de la clase GestionarComicPOJOTEST de tipo de la clase ComicDTO
+	 */
 	List<ComicDTO> listaComic=null;
+	/**
+	 * 
+	 * Metodo encargado de ejecutar esto antes que cualquier test
+	 * <b>Caso de Uso</b>
+	 * @author santi
+	 *
+	 */
 	@BeforeTest
 	public void before() {
 		gestionarComicPOJO= new GestionarComicPOJO();
@@ -34,19 +50,31 @@ public class GestionarComicPOJOTest {
 				TematicaEnum.CIENCIA_FICCION, "BIBLIOTECA MARVEL", 128, new BigDecimal(2345),
 				" Roger Stern", Boolean.TRUE, LocalDate.now(), EstadoEnum.ACTIVO, 6L);
 		gestionarComicPOJO.agregarComicDTOLista(comicDTO);
+		
+		gestionarComicPOJO= new GestionarComicPOJO();
+		comicDTO = gestionarComicPOJO.crearComicDTO("2", "the spectacular spiderman V2-usa	", "planeta Comics",
+				TematicaEnum.CIENCIA_FICCION, "MARVEL COMICS", 208, new BigDecimal(6225),
+				" Straczynski,Deodato Jr.,Barnes,Eaton", Boolean.TRUE, LocalDate.now(), EstadoEnum.INACTIVO, 0L);
+		gestionarComicPOJO.agregarComicDTOLista(comicDTO);
 		creartComicDTOTest();
 		listaComic=gestionarComicPOJO.getListaComics();
 	}
 	
-	@Test(enabled = false)
-	public void crearComicDTOTest() {
-		GestionarComicPOJO gestionarComicPOJO = new GestionarComicPOJO();
-		gestionarComicPOJO.crearComicDTO();
-		Assert.assertNotNull(gestionarComicPOJO.getListaComics());
-		Assert.assertTrue(gestionarComicPOJO.getListaComics().size() != 0);
-		Assert.assertTrue(!gestionarComicPOJO.getListaComics().isEmpty());
-	}
-
+//	@Test(enabled = false)
+//	public void crearComicDTOTest() {
+//		GestionarComicPOJO gestionarComicPOJO = new GestionarComicPOJO();
+//		gestionarComicPOJO.crearComicDTO();
+//		Assert.assertNotNull(gestionarComicPOJO.getListaComics());
+//		Assert.assertTrue(gestionarComicPOJO.getListaComics().size() != 0);
+//		Assert.assertTrue(!gestionarComicPOJO.getListaComics().isEmpty());
+//	}
+	/**
+	 * 
+	 * Metodo encargado de probar el metodo crearComicDTOTest
+	 * <b>Caso de Uso</b>
+	 * @author santi
+	 *
+	 */
 	@Test (enabled=false)
 	public void creartComicDTOTest() {
 
