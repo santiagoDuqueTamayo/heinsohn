0ee84e24ddee0080d515233abdb5ca9d2e24817b
Santiago Duque Tamayo <santiagohdt@gmail.com>
2019-11-13 17:42:47 -0500

    metodos del crud con REST

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/rest/GestionarComicRest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/rest/GestionarComicRest.java
@@ -3,6 +3,9 @@
  */
 package com.hbt.semillero.rest;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.PATCH;
@@ -13,6 +16,8 @@ import javax.ws.rs.core.MediaType;
 
 import com.hbt.semillero.dto.ComicDTO;
 import com.hbt.semillero.ejb.IGestionarComicLocal;
+import com.hbt.semillero.entidades.EstadoEnum;
+import com.hbt.semillero.entidades.TematicaEnum;
 
 /**
  * <b>Descripción:<b> utilizamos gestionarComicBean con el mundo exterior
@@ -31,7 +36,15 @@ public class GestionarComicRest  {
 	public String primerRest() {
 		return "hola mundo, esto es un cambio";
 	}
-	
+	/**
+	 * 
+	 * Metodo encargado de consultar un comic en la base de datos y enviarlo como un Json
+	 * <b>Caso de Uso</b>
+	 * @author santi
+	 * 
+	 * @param idComic
+	 * @return
+	 */
 	@GET
 	@Path("/consultarComic")
 	@Produces(MediaType.APPLICATION_JSON)//convierte el mensaje a JSON
@@ -41,6 +54,43 @@ public class GestionarComicRest  {
 		}
 		return null;
 	}
+
+	@GET
+	@Path("/eliminarComic")
+	@Produces(MediaType.APPLICATION_JSON)//convierte el mensaje a JSON
+	public void eliminarComic(@QueryParam("idComic") Long idComic)  {
+		if (idComic!=null) {
+			gestionarComicEJB.eliminarComic(idComic);
+		}
+	}
 	
-	
+	@GET
+	@Path("/crearComic")
+	@Produces
+	public String crearComic(@QueryParam("idComic") String idComic, @QueryParam("nombre") String nombre,@QueryParam("editorial") String editorial,
+			@QueryParam("tematica")TematicaEnum tematica,@QueryParam("estado") EstadoEnum estado, @QueryParam("coleccion")String coleccion,@QueryParam("numeroPaginas") Integer numeroPaginas, 
+			@QueryParam("precio") BigDecimal precio, @QueryParam("color")boolean color,@QueryParam("autores") String autores,@QueryParam("fecha")String fecha,@QueryParam("cantidad") Long cantidad ) {
+			
+		LocalDate localDate;
+		ComicDTO comicDTO= new ComicDTO(idComic, nombre, editorial, tematica, coleccion, numeroPaginas, precio, color, autores,localDate = LocalDate.parse(fecha), estado, cantidad);
+			gestionarComicEJB.crearComic(comicDTO);
+		return "se creo el elemento en la bd";
+	}
+	@GET
+	@Path("/modificarComic")
+	@Produces(MediaType.APPLICATION_JSON)//convierte el mensaje a JSON
+	public void modificarComic(@QueryParam("idComic") String idComic, @QueryParam("nombre") String nombre,
+			@QueryParam("editorial") String editorial,
+			@QueryParam("tematica")TematicaEnum tematica,@QueryParam("estado") EstadoEnum estado, 
+			@QueryParam("coleccion")String coleccion,@QueryParam("numeroPaginas") Integer numeroPaginas, 
+			@QueryParam("precio") BigDecimal precio, @QueryParam("color")boolean color,
+			@QueryParam("autores") String autores,@QueryParam("fecha")String fecha,
+			@QueryParam("cantidad") Long cantidad)  {
+		
+		if (idComic!=null) {
+			LocalDate localDate;
+			ComicDTO comicDTO= new ComicDTO(idComic, nombre, editorial, tematica, coleccion, numeroPaginas, precio, color, autores,localDate = LocalDate.parse(fecha), estado, cantidad);
+			gestionarComicEJB.modificarComic(Long.parseLong(idComic), nombre, comicDTO);
+	}
+	}
 }
